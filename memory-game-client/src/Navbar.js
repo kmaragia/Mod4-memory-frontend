aadd09d08795c594ae8b349aee6244a6ba740729
Natany Caxeta <natanycaxeta@icloud.com>
2019-07-25 17:55:08 -0400

    some frontend work

---
+++ memory-game-client/src/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Menu } from 'semantic-ui-react' /* FROM SEMANTIC REACT*/ 
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <div>
+                <div>
+                    Game app
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Navbar
